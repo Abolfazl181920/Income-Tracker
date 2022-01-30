9c84aa16fcb2006456c2842703e8e222b124c029
Abolfazl <abolfazlm181920@gmail.com>
2022-01-30 20:03:48 +0330

    add Refs to inputs & clean code

--- src/Components/IncomeForm.js
+++ src/Components/IncomeForm.js
@@ -3,6 +3,8 @@ import React, { useRef } from 'react';
 const IncomeForm = ({ income, setIncome }) => {
 
     const description = useRef(null);
+    const date = useRef(null);
+    const price = useRef(null);
 
     const addIncome = (event) => {
         event.preventDefault();
@@ -11,10 +13,31 @@ const IncomeForm = ({ income, setIncome }) => {
     return (
         <form className="income-form" onSubmit={addIncome}>
             <div className="form-inner">
-                <input ref={description} type="text" id="desc" name="desc" placeholder="Income Description..." />
-                <input type="number" id="price" name="price" placeholder="Price..." />                
-                <input type="date" id="date" name="date" placeholder="Income Description" placeholder="Income Date..." />
-                <input type="submit" value="Add Income" />
+                <input ref={description}
+                    type="text"
+                    id="desc"
+                    placeholder="Income Description..."
+                />
+
+                <input
+                    ref={price}
+                    type="number"
+                    id="price"
+                    placeholder="Price..."
+                />
+
+                <input
+                    ref={date}
+                    type="date"
+                    id="date"
+                    placeholder="Income Description"
+                    placeholder="Income Date..."
+                />
+                
+                <input
+                    type="submit"
+                    value="Add Income"
+                />
             </div>
         </form>
     )
