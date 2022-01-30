5f6a8342838801816fb22d21143470b7acc87f0a
Abolfazl <abolfazlm181920@gmail.com>
2022-01-30 19:55:27 +0330

    use useRef for income input

--- src/Components/IncomeForm.js
+++ src/Components/IncomeForm.js
@@ -1,10 +1,13 @@
 import React, { useRef } from 'react';
 
 const IncomeForm = ({ income, setIncome }) => {
+
+    const description = useRef(null);
+
     return (
         <form className="income-form">
             <div className="form-inner">
-                <input type="text" id="desc" name="desc" placeholder="Income Description..." />
+                <input ref={description} type="text" id="desc" name="desc" placeholder="Income Description..." />
                 <input type="number" id="price" name="price" placeholder="Price..." />                
                 <input type="date" id="date" name="date" placeholder="Income Description" placeholder="Income Date..." />
                 <input type="submit" value="Add Income" />
