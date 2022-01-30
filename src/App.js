e3a5b66e2344bf90106ab6a5debcf95fa38b494d
Abolfazl <abolfazlm181920@gmail.com>
2022-01-30 19:01:01 +0330

    add 2 states for incomes and totalincomes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
 const App = () => {
+
+  const [ income, setIncome ] = useState([]);
+  const [ totalIncome, setTotalIncome ] = useState(0);
+  
   return (
     <div className="App"></div>
   );
