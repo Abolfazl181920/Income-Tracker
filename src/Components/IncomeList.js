1bb863e322424a922a1e5be881c23b3903395771
Abolfazl <abolfazlm181920@gmail.com>
2022-01-30 20:51:40 +0330

    filter arrays and set states

--- src/Components/IncomeList.js
+++ src/Components/IncomeList.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
+import IncomeItem from './IncomeItem';
+
 const IncomeList = ({ income, setIncome }) => {
+
+    const removeIncome = (i) => {
+        let temp = income.filter((value, index) => index != i);
+        setIncome(temp);
+    }
     return(
         <div className="income-list"></div>
     );
