e842a754fcfc0ea9db9e578d4d3ed9c5f0f184d1
Abolfazl <abolfazlm181920@gmail.com>
2022-01-30 19:03:19 +0330

    create Header component and send props from App to Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 
+import Header from './Components/Header';
+
 const App = () => {
 
   const [ income, setIncome ] = useState([]);
   const [ totalIncome, setTotalIncome ] = useState(0);
-  
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Header totalIncome={totalIncome} />
+    </div>
   );
 }
 
