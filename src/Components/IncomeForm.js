4f9956c82ff4f05f04c3f9ad92d9e617243716df
Abolfazl <abolfazlm181920@gmail.com>
2022-01-30 19:18:22 +0330

    add statics of Income component

--- src/Components/IncomeForm.js
+++ src/Components/IncomeForm.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 function IncomeForm() {
     return (
-        <div>
-            
-        </div>
+        <form className="income-form">
+            <div className="form-inner">
+                <input type="text" id="desc" name="desc" placeholder="Income Description..." />
+                <input type="number" id="price" name="price" placeholder="Price..." />                
+                <input type="date" id="date" name="date" placeholder="Income Description" placeholder="Income Date..." />
+                <input type="submit" value="Add Income" />
+            </div>
+        </form>
     )
 }
 
