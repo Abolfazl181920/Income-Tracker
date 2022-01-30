6a36ff43169a8d3a0c4fc1885236b79b20af8d96
Abolfazl <abolfazlm181920@gmail.com>
2022-01-30 20:41:22 +0330

    add variables in IncomeItem component

--- src/Components/IncomeItem.js
+++ src/Components/IncomeItem.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
-const IncomeItem = () => {
+const IncomeItem = ({ income, index, removeIncome }) => {
+
+    let data = new Date(income.Date);
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
     return (
         <div>
             
